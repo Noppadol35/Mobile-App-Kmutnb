e2ab8bb6e14e756a75d6553e84e1b756cfb58fab
Noppadol35 <s6504062620191@email.kmutnb.ac.th>
2024-12-18 11:01:49 +0700

    week3-18-12-2024

---
+++ week3-18-12-2024/lib/main.dart
@@ -0,0 +1,335 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "EV Charge Australia",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: "EV Charge Australia"),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool _isNotificationOn = true;
+
+  void _toggleNotification() {
+    setState(() {
+      _isNotificationOn = !_isNotificationOn;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          widget.title,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 26,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        leading: IconButton(
+          onPressed: () {
+            debugPrint("Menu");
+          },
+          icon: const Icon(Icons.menu, color: Colors.white),
+          tooltip: "Menu",
+        ),
+        actions: [
+          IconButton(
+            onPressed: _toggleNotification,
+            icon: Icon(
+              _isNotificationOn
+                  ? Icons.notifications_rounded
+                  : Icons.notifications_off_rounded,
+              color: Colors.white,
+            ),
+            tooltip: "Notification",
+          ),
+          IconButton(
+            onPressed: () {
+              debugPrint("Search");
+            },
+            icon: const Icon(
+              Icons.search,
+              color: Colors.white,
+            ),
+            tooltip: "Search",
+          ),
+        ],
+        backgroundColor: const Color.fromARGB(255, 2, 23, 127),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.network(
+                "https://evchargeaustralia.com.au/wp-content/uploads/2021/06/Logo.png",
+                height: 150,
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                "Thank you for using our service",
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                "We are happy to be a part of your journey.",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.normal,
+                  color: Colors.black,
+                ),
+              ),
+              const SizedBox(height: 30),
+
+              // Card
+              const Card(
+                child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Icon(Icons.account_balance_wallet_rounded,
+                              color: Colors.grey),
+                          SizedBox(width: 10),
+                          Expanded(
+                            child: Text(
+                              "Details Details Details Details Details ",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Icon(Icons.calendar_month_outlined,
+                                  color: Colors.blue),
+                              SizedBox(width: 10),
+                              Text(
+                                "Date",
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                          Text(
+                            "11 Dec 2024",
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Icon(Icons.ev_station_rounded,
+                                  color: Colors.green),
+                              SizedBox(width: 10),
+                              Text(
+                                "Station",
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                          Text(
+                            "Ev Charge Australia",
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Icon(Icons.electrical_services_rounded,
+                                  color: Colors.orange),
+                              SizedBox(width: 10),
+                              Text(
+                                "Typing",
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                          Text(
+                            "CCS2",
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Icon(Icons.access_time_filled,
+                                  color: Colors.blue),
+                              SizedBox(width: 10),
+                              Text(
+                                "Time",
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                          Text(
+                            "00:11:11",
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Icon(Icons.electric_bolt_rounded,
+                                  color: Color.fromARGB(255, 243, 188, 49)),
+                              SizedBox(width: 10),
+                              Text(
+                                "Unit",
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                          Text(
+                            "12.314 kWh",
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(
+                        Icons.monetization_on,
+                        color: Colors.green,
+                        size: 30,
+                      ),
+                      SizedBox(width: 10),
+                      Text(
+                        "Total",
+                        style: TextStyle(
+                          fontSize: 26,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        "23.45",
+                        style: TextStyle(
+                          fontSize: 26,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Icon(Icons.attach_money, color: Colors.green),
+                    ],
+                  )
+                ],
+              ),
+              //saclloview
+
+              const SizedBox(height: 50),
+              const TextField(
+                decoration: InputDecoration(
+                  labelText: "Enter your name",
+                  hintText: "Noppadol P.",
+                  prefixIcon: Icon(Icons.person),
+                  suffixIcon: Icon(Icons.clear),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+
+              const SizedBox(height: 20),
+              const TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: "Enter your password",
+                  hintText: "Admin123",
+                  prefixIcon: Icon(Icons.lock),
+                  suffixIcon: Icon(Icons.remove_red_eye),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          debugPrint("Hello!");
+        },
+        backgroundColor: Colors.blue,
+        child: const Icon(Icons.home),
+      ),
+    );
+  }
+}
